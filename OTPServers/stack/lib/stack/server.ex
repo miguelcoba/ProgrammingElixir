6aab6e5478535b0d1ac44da6c67e6c44950cab03
Miguel Coba <miguel.coba@gmail.com>
2016-01-18 02:25:12 +0000

    16.2 answer

--- OTPServers/stack/lib/stack/server.ex
+++ OTPServers/stack/lib/stack/server.ex
@@ -4,4 +4,8 @@ defmodule Stack.Server do
 	def handle_call(:pop, _from, [head | tail]) do
 		{ :reply, head, tail }
 	end
+
+	def handle_cast({:push, value}, stack) do
+		{ :noreply, [value|stack]}
+	end
 end
