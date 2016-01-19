fce9a1868aeab30612695e241a06ba0dbea3c3f1
Miguel Coba <miguel.coba@gmail.com>
2016-01-19 04:29:03 +0000

    Chapter 16 answers

--- OTPServers/sequence/lib/sequence/server.ex
+++ OTPServers/sequence/lib/sequence/server.ex
@@ -1,15 +1,27 @@
 defmodule Sequence.Server do
 	use GenServer
 
+	def start_link(current_number) do
+		GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+	end
+
+	def next_number do
+		GenServer.call __MODULE__, :next_number
+	end
+
+	def increment_number(delta) do
+		GenServer.cast __MODULE__, {:increment_number, delta}
+	end
+
 	def handle_call(:next_number, _from, current_number) do
 		{ :reply, current_number, current_number + 1 }
 	end
 
-	def handle_call({:set_number, new_number}, _from, _current_number) do
-		{ :reply, new_number, new_number }
+	def handle_cast({:increment_number, delta}, current_number) do
+		{ :noreply, current_number + delta}
 	end
 
-	def handle_call({:factors, number}, _, _) do
-		{ :reply, { :factors_of, number, factors(number) }, [] }
+	def format_status(_reason, [ _pdict, state ]) do
+		[data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
 	end
 end
--- OTPServers/stack/lib/stack/server.ex
+++ OTPServers/stack/lib/stack/server.ex
@@ -1,11 +1,47 @@
 defmodule Stack.Server do
 	use GenServer
 
+	def start_empty do
+		start_link([])
+	end
+
+	def shutdown do
+		System.halt(1)
+	end
+
+	def start_with_random_data do
+		start_link([1, "abc", 2, "cdf", 3])
+	end
+
+	def start_link(initial_values) do
+		GenServer.start_link(__MODULE__, initial_values, name: __MODULE__)
+	end
+
+	def pop do
+		GenServer.call(__MODULE__, :pop)
+	end
+
+	def push(value) do
+		GenServer.cast(__MODULE__, {:push, value})
+	end
+
+	def status do
+		:sys.get_status __MODULE__
+	end
+
 	def handle_call(:pop, _from, [head | tail]) do
 		{ :reply, head, tail }
 	end
 
 	def handle_cast({:push, value}, stack) do
 		{ :noreply, [value|stack]}
+	end
+
+	def format_status(_reason, [ _pdict, state ]) do
+		[data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
+	end
+
+	def terminate(reason, state) do
+		IO.puts "Terminated with reason: #{reason}. Final state: #{state}"
 	end
 end
