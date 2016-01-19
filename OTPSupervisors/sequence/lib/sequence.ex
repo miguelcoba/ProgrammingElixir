f18fda95ebdcb2ff0ef2ea38b23db9e9e49dde97
Miguel Coba <miguel.coba@gmail.com>
2016-01-19 06:04:04 +0000

    Chapter 18 answers

--- OTPSupervisors/sequence/lib/sequence.ex
+++ OTPSupervisors/sequence/lib/sequence.ex
@@ -4,6 +4,6 @@ defmodule Sequence do
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    {:ok, _pid} = Sequence.Supervisor.start_link(123)
+    {:ok, _pid} = Sequence.Supervisor.start_link(Application.get_env(:sequence, :initial_number))
   end
 end
--- OTPSupervisors/sequence/mix.exs
+++ OTPSupervisors/sequence/mix.exs
@@ -15,7 +15,9 @@ defmodule Sequence.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
-     mod: {Sequence, []}]
+     mod: {Sequence, []},
+     env: [ initial_number: 456],
+     registered: [ :sequence ]]
   end
 
   # Dependencies can be Hex packages:
