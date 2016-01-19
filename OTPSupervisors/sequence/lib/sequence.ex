94e59c092dd5fd0b259b59ac3a48b7c26847330c
Miguel Coba <miguel.coba@gmail.com>
2016-01-19 05:20:13 +0000

    Chapter 17 answers

--- OTPSupervisors/sequence/lib/sequence.ex
+++ OTPSupervisors/sequence/lib/sequence.ex
@@ -4,15 +4,6 @@ defmodule Sequence do
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
-    children = [
-      worker(Sequence.Server, [123]),
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Sequence.Supervisor]
-    Supervisor.start_link(children, opts)
+    {:ok, _pid} = Sequence.Supervisor.start_link(123)
   end
 end
